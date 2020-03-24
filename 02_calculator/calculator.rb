7e9b5c69422a78cc696118369f0953a5268d8823
Ariel <arielrt20@gmail.com>
2020-03-24 16:56:44 -0400

    sc_commit

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -10,11 +10,36 @@ end
 def sum(numbers)
     numbers.inject(0){|total, number| total+number}
 end
- 
-def factorial x
-    if x <= 1
-        1
-    else 
-        x * factorial(x - 1)
+
+def multiply(arr)
+    product = 1
+    arr.each do |num|
+        product = product * num
     end
+    product
+end
+
+def power(num, exponent)
+    total = num
+    if exponent == 0
+        return 1
+    else
+        (exponent - 1).times do
+            total = total * num
+        end
+    end
+    total
+end
+
+def factorial(num)
+    total = num
+    if num == 0
+        return 1
+    else
+        (num - 1).times do
+            num = num - 1
+            total = total * num
+        end
+    end
+    total
 end
